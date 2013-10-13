06e2d37977e1a045064523197edc1bd8c077d5d6
koroandr <koroandr@gmail.com>
2013-10-13 04:25:11 +0400

    Убрал мусор из main

--- main.js
+++ main.js
@@ -12,22 +12,5 @@ require(
     function(data, $, Main, radio, router) {
         Main("body", data);
         router.init();
-
-
-        var lectors = data.lectures.lectors;
-        lectors = lectors.map(function(l){l.shri2013 = []; return l});
-        var getById = function(id) {
-            for (var i = 0; i < lectors.length; i++) {
-                if (lectors[i].id == id) {
-                    return lectors[i];
-                }
-            }
-        }
-        for (var i = 0; i < data.lectures.lectures.length; i++) {
-            getById(data.lectures.lectures[i].lector_id).shri2013.push(data.lectures.lectures[i]);
-        }
-
-        window.lect = lectors;
-
     }
 );
