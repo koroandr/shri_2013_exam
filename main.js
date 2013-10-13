63c1cd07b9d7785d702cef0e07a8a9388e0a408b
koroandr <koroandr@gmail.com>
2013-10-13 14:45:47 +0400

    Кеширование данных приложени я в localStorage

--- main.js
+++ main.js
@@ -3,14 +3,34 @@
  */
 require(
     [
-        "data",
         "lib/jquery",
         "blocks/main/main",
         "lib/radio",
         "app/router"
     ],
-    function(data, $, Main, radio, router) {
-        Main("body", data);
-        router.init();
+    function($, Main, radio, router) {
+
+        function init(data) {
+            Main("body", data);
+            router.init();
+        }
+
+        var data;
+        if (typeof window.localStorage != 'undefined') {
+            data = JSON.parse(localStorage.getItem("data"));
+        }
+
+        if (!data || data == "null") {
+            require(["data"], function(data){
+                if (typeof window.localStorage != 'undefined') {
+                    localStorage.setItem("data", JSON.stringify(data));
+                }
+                init(data);
+            });
+        } else {
+            init(data);
+        }
+
+
     }
 );
